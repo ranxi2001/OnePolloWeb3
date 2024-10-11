0bf9191181979cea26509bfc4843a52a5e27ec9a
ranxi2001 <ranxi169@163.com>
2024-10-11 15:24:18 +0800

    fix 自动超链接 update 更新说明

--- _plugins/md_to_html.rb
+++ _plugins/md_to_html.rb
@@ -2,7 +2,16 @@ module Jekyll
   module MdToHtml
     def md_to_html(input)
       Jekyll.logger.info "MdToHtml Plugin", "Replacing .md links with .html"
-      input.gsub(/\.md(?=["')\s])/i, '.html')
+
+      # 首先替换 .md 为 .html
+      input = input.gsub(/\.md(?=["')\s])/i, '.html')
+
+      # 匹配纯文本 URL 并自动转换为 HTML 超链接，避免重复转换已经是链接的部分
+      input.gsub!(%r{(?<!href=['"])https?://[^\s<>\n]+}) do |url|
+        "<a href='#{url}'>#{url}</a>"
+      end
+
+      input # 返回修改后的输入
     end
   end
 end
